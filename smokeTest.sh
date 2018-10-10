67a4369dc453f26946e737b067e63e0e2ede9ff3
Clemens Anzmann <clemensa@colbenson.com>
2018-10-10 14:55:15 +0100

    update smoke test

--- smokeTest.sh
+++ smokeTest.sh
@@ -1,15 +1,17 @@
 set -e
 
-echo 'Node version:'
-node -v
-echo '-------------------'
+echo 'Running smoke test...'
 
-node main.js 0
-node main.js 1
-node main.js 9
-node main.js 10
-node main.js 21
-node main.js 57
-node main.js 98
+echo "0 => \c" && node main.js 0
+echo "1 => \c" && node main.js 1
+echo "9 => \c" && node main.js 9
+echo "10 => \c" && node main.js 10
+echo "21 => \c" && node main.js 21
+echo "57 => \c" && node main.js 57
+echo "98 => \c" && node main.js 98
+echo "111 => \c" && node main.js 111
+echo "198 => \c" && node main.js 198
+echo "398 => \c" && node main.js 398
+echo "999 => \c" && node main.js 999
 
-echo '...no errors thrown, smoke-test passed.'
+echo '...no errors thrown, check result above.'
