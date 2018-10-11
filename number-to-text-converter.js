fedbed6cbccaf1081103fe5e4a2128d78b0c2872
Clemens Anzmann <clemensa@colbenson.com>
2018-10-11 13:02:57 +0100

    refactor code

--- number-to-text-converter.js
+++ number-to-text-converter.js
@@ -2,7 +2,7 @@ const below20s = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven',
 const tenners = ['zero', 'ten', 'twenty', 'thirty', 'fourty', 'fivety', 'sixty', 'seventy', 'eighty', 'ninety'];
 
 const argumentIsValid = argument => {
-  if (!argument || isNaN(argument)) {
+  if (!argument || isNaN(parseInt(argument))) {
     throw('Argument must be a number');
   }
   if (argument > 999999) {
@@ -11,24 +11,23 @@ const argumentIsValid = argument => {
   return true;
 }
 
-const processSingleDigitNumber = number => {
+const processNumberBelow20 = number => {
   return below20s[parseInt(number)];
 }
 
 const processDoubleDigitNumber = number => {
-  const integerNumber = parseInt(number);
-  if (integerNumber < 20) {
-    return processSingleDigitNumber(number);
+  if (parseInt(number) < 20) {
+    return processNumberBelow20(number);
   }
-  const digits = number.split('');
+  const digits = number.toString().split('');
   const firstHalf = digits[0] !== '0' ? tenners[digits[0]] : '';
-  const secondHalf = digits[1] !== '0' ? `-${processSingleDigitNumber(digits[1])}` : '';
+  const secondHalf = digits[1] !== '0' ? `-${processNumberBelow20(digits[1])}` : '';
   return `${firstHalf}${secondHalf}`;
 }
 
 const processTripleDigitNumber = number => {
   const digits = number.split('');
-  const hundredText = digits[0] !== '0' ? `${processSingleDigitNumber(digits[0])} hundred and ` : '';
+  const hundredText = digits[0] !== '0' ? `${processNumberBelow20(digits[0])} hundred and ` : '';
   const ntyText = processDoubleDigitNumber(
     String(digits[1]+digits[2])
   );
@@ -36,49 +35,56 @@ const processTripleDigitNumber = number => {
 }
 
 const processSegment = segmentString => {
-  const digits = segmentString.split('');
-  if (digits.length === 1) {
-    return processSingleDigitNumber(segmentString);
-  }
-  if (digits.length === 2) {
-    return processDoubleDigitNumber(segmentString);
-  }
-  if (digits.length === 3) {
-    return processTripleDigitNumber(segmentString);
+  switch (segmentString.split('').length) {
+    case 1:
+      return processNumberBelow20(segmentString);
+    case 2:
+      return processDoubleDigitNumber(segmentString);
+    case 3:
+      return processTripleDigitNumber(segmentString);
+    default:
+      return '';
   }
 }
 
-const processTripleStringArray = segmentStringArray => {
-  const length = segmentStringArray.length;
-  if (length === 1) {
-    return segmentStringArray[0];
-  }
-  if (length === 2) {
-    const thousandString = segmentStringArray[0] && segmentStringArray[0] !== 'zero' ?`${segmentStringArray[0]} thousand and ` : '';
-    return `${thousandString}${segmentStringArray[1]}`;
+const processSegmentStringArray = segmentStringArray => {
+  switch (segmentStringArray.length) {
+    case 1:
+      return segmentStringArray[0];
+    case 2:
+      const thousandString = segmentStringArray[0] && segmentStringArray[0] !== 'zero' ?`${segmentStringArray[0]} thousand and ` : '';
+      return `${thousandString}${segmentStringArray[1]}`;
+    default:
+      return '';
   }
 }
 
 const processEuros = euros => {
-  const triples = euros.toString().match(/.{1,3}/g);
-  const segmentStringArray = triples.map(segmentString => {
-    return processSegment(segmentString);
-  });
-  return `${processTripleStringArray(segmentStringArray)} euros`;
+  const threeDigitSegments = euros.toString().match(/.{1,3}/g);
+  const segmentStringArray = threeDigitSegments.map(segmentString => processSegment(segmentString));
+  const euroString = processSegmentStringArray(segmentStringArray);
+  const euroSuffix = euroString !== 'one' ? 'euros' : 'euro';
+  return `${euroString} ${euroSuffix}`;
 }
 
 const processCents = cents => {
   const centString = cents ? processDoubleDigitNumber(cents) : 'zero';
-  return ` and ${centString || 'zero'} cents`; // TODO: should unexapected result default to 'zero cents'?
+  const centSuffix = centString !== 'one' ? 'cents' : 'cent';
+  return ` and ${centString || 'zero'} ${centSuffix}`;
+}
+
+const getEuroAndCentValue = argument => {
+  const splitNumber = argument.toString().split('.');
+  const euros = parseInt(splitNumber[0]);
+  const cents = parseInt(splitNumber[1] ? `${splitNumber[1]}0` : '0');
+  return { euros, cents };
 }
 
 const argument = process.argv[2];
 
 if (argumentIsValid(argument)) {
-  const splitNumber = argument.split('.');
-  const euros = splitNumber[0];
-  const cents = splitNumber[1] && splitNumber[1].length === 1 ? splitNumber[1]+'0' : splitNumber[1]; // TODO: should be its own function
-  const euroString = processEuros(euros);
-  const centString = processCents(cents);
+  const parsedCurrencyValue = getEuroAndCentValue(argument);
+  const euroString = processEuros(parsedCurrencyValue.euros);
+  const centString = processCents(parsedCurrencyValue.cents);
   console.log(euroString+centString);
 }
